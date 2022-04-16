dc5f7e04d1ec95e3a33d01fa3c2fb017aea77744
sonya-c <sonya.castro.gomez@gmail.com>
2022-04-16 15:17:24 -0500

    feat: display table

--- modules/data_visualization.py
+++ modules/data_visualization.py
@@ -1,4 +1,16 @@
 
+import pandas as pd
+
+
+def table(data_file: str, title: str):
+    data = pd.read_csv(data_file, encoding='unicode_escape')  # Get the data
+    print(F"TABLE: {title}")
+
+    # more options can be specified also
+    with pd.option_context('display.max_rows', None):
+        # Display all the information
+        print(data)
+
+
 def visualizate():
-    # Todo
-    pass
+    table("./data/data.csv", "raw data")
