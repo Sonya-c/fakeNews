32fe83b470cca7a2e87b9e6cffaf27b67f514e70
sonya-c <sonya.castro.gomez@gmail.com>
2022-04-24 15:49:30 -0500

    feat: generate sentences

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from modules import scrape, analysis, visualizate, generate
 NUM_PAGES: int = int(input("Number of pages = ") or "100")  # Defaults to 100
 
 print("Scraping el sitio web...")
-scrape(NUM_PAGES)
+# scrape(NUM_PAGES)
 
 print("\nAnalizando las páginas...")
-analysis()
+# analysis()
 
 print("\nGenerando los titulares...")
-# generate()
+generate()
 
 print("\nPreparando la visualización...")
-visualizate()
+# visualizate()
--- modules/generate_title.py
+++ modules/generate_title.py
@@ -1,4 +1,68 @@
+import csv
+from typing import List, Tuple
+import random
+
+from numpy import save
+from modules.to_csv import save_table
+
+
+def get_next(next_words):
+    total = sum(w for _, w in next_words)
+    rand = random.uniform(0, total)  # rand number frm uniform distribution
+    cumulative = 0  # Cumulative frecuency
+
+    for next_word, frecuency in next_words:
+        cumulative += frecuency
+
+        if cumulative > rand:
+            return next_word[1]
+
+
+def generate_sentence(n_gram, start_word, n=50) -> List[str]:
+    sentence: str = start_word
+
+    for _ in range(n):
+        next_words = [word for word in n_gram if word[0][0] == start_word]
+
+        if not next_words:
+            break
+
+        start_word = get_next(next_words).replace(" ", "")
+
+        if (start_word != ""):
+            sentence += " " + start_word
+
+    return sentence
+
+
+def get_data(file_name: str) -> List[str]:
+    data: List[str] = []
+
+    with open(f"data/exp_1/{file_name}", "r") as file:
+        reader = csv.reader(file)
+        next(reader, None)  # skip the headers
+
+        for row in reader:
+            data.append(row)
+
+    return data
+
 
 def generate():
-    # todo
-    pass
+    bigrams = [(tuple(row[0].split(" ")), int(row[1]))
+               for row in get_data("bigrams.csv")]
+    words = [row[0] for row in get_data("word_frecuency.csv")]
+
+    sentences: List[str] = []
+    i: int = 0
+    j: int = 0
+    while (j <= 30 and i < len(words)):
+        sentence = generate_sentence(bigrams, words[i], 20)
+
+        if (len(sentence.split(" ")) > 3):
+            sentences.append([sentence])
+            j += 1
+
+        i += 1
+
+    save_table("./data/exp_2/data.csv", ["title"], sentences)
