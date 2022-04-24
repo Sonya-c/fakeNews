98b55d3fb53c6250531c5d4842fba6a3b20580db
sonya-c <sonya.castro.gomez@gmail.com>
2022-04-23 22:49:43 -0500

    feat: bigrams and trigrams

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -26,7 +26,7 @@ def clean_row(sentence: str, stop_words):
 
 def get_word_type(word_type: Dict[str, int], words_list) -> Dict[str, int]:
     tagged = nltk.pos_tag(words_list)
-    
+
     for tag in tagged:
         word_type[tag[1]] = word_type.get(tag[1], 0) + 1
 
@@ -39,33 +39,45 @@ def get_word_count(row: str) -> Dict[str, int]:
     for word in row:
         word = word.replace(":", "").replace("'", "")
 
-        if  word != "": 
+        if word != "":
             word_count[word] = word_count.get(word, 0) + 1  # increment
-    
+
     return word_count
 
 
 def merge_dict(d1: Dict[str, int], d2: Dict[str, int]) -> Dict[str, int]:
     for key in d2:
         d1[key] = d2[key] + d1.get(key, 0)
-    
+
     return d1
 
 
 def get_max(date: str, word_count: Dict[str, int], word_date: Tuple[str, int]) -> Tuple[str, int]:
     max_word = max(word_count, key=word_count.get)
     max_value = max(word_count.values())
-    
+
     wd = word_date.get(date, ["", 0])  # Get the word count
-    
+
     if (max_word == wd[0]):
         max_value += wd[1]
         wd = [max_word, max_value]
-    
+
     elif (max_value > wd[1]):
         wd = [max_word, max_value]
 
     return wd
+
+
+def dict_to_list(d: Dict[str, List[any]]) -> List[any]:
+    l = []
+
+    for key in d:
+        l.append(
+            [key] + d[key]
+        )
+
+    print(l)
+    return l
 
 
 def save_table(file_name: str, headers: List[str], rows: List[any]):
@@ -75,12 +87,30 @@ def save_table(file_name: str, headers: List[str], rows: List[any]):
         writer.writerows(rows)
 
 
+def n_grams(ngrams, word_list, n=1):
+    temp = zip(*[word_list[i:] for i in range(0, n)])
+    ng = [' '.join(ngram) for ngram in temp]
+
+    for ngram in ng:
+        ngrams[ngram] = ngrams.get(ngram, 0) + 1
+
+    return ngrams
+
+
+def sort_dict(d: Dict[any, any]):
+    return dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+
+
 def analysis():
     init()
     word_count: Dict[str, int] = {}  # word, word conuter, date
-    word_date: Dict[str, Tuple[str, int]] = {}  # Date, most used word and the date
+    # Date, most used word and the date
+    word_date: Dict[str, Tuple[str, int]] = {}
     article_date: Dict[str, int] = {}  # Date, number of articles
     word_type: Dict[str, int] = {}  # Word type and the frecuency
+
+    bigram: Dict[str, List[int]] = {}
+    trigram: Dict[str, List[int]] = {}
 
     stop_words = get_stop_words()
 
@@ -89,8 +119,12 @@ def analysis():
         next(reader, None)  # skip the headers
 
         for row in reader:
-            if (row == []): continue  # skips the empy rows
+            if (row == []):
+                continue  # skips the empy rows
             word_list = clean_row(row[0], stop_words)
+
+            bigram = n_grams(bigram, word_list, 2)
+            trigram = n_grams(trigram, word_list, 3)
 
             word_type = get_word_type(word_type, word_list)
 
@@ -101,12 +135,23 @@ def analysis():
 
             if (date != "ERROR"):  # Si la fecha es correcta, añadir un articulo a esta fecha
                 article_date[date] = article_date.get(date, 0) + 1
-                word_date[date] = get_max(date, wc, word_date)  # Contar la palabra mas usada en esa fecha
-            
-    # print(word_count, word_date, article_date, sep="\n")
-    word_count = dict(sorted(word_count.items(), key=lambda item: item[1]))
+                # Contar la palabra mas usada en esa fecha
+                word_date[date] = get_max(date, wc, word_date)
 
-    save_table("word_type.csv", ["word_type", "count"], list(word_type.items()))
-    save_table("word_frecuency.csv", ["word", "frecuency"], list(word_count.items()))
-    save_table("word_date.csv", ["date", "word_frecuency"], list(word_date.items()))
-    save_table("article_date.csv", ["date", "articles"], list(article_date.items()))
+    # print(word_count, word_date, article_date, sep="\n")
+    word_count = sort_dict(word_count)
+    bigram = sort_dict(bigram)
+    trigram = sort_dict(trigram)
+
+    save_table("word_type.csv", ["word_type",
+               "count"], list(word_type.items()))
+    save_table("word_frecuency.csv", [
+               "word", "frecuency"], list(word_count.items()))
+    save_table("word_date.csv", [
+               "date", "word, frecuency"], dict_to_list(word_date))
+    save_table("article_date.csv", [
+               "date", "articles"], list(article_date.items()))
+    save_table("bigrams.csv", [
+               "bigram", "frecuency"], list(bigram.items()))
+    save_table("trigrams.csv", [
+               "trigram", "frecuency"], list(trigram.items()))
